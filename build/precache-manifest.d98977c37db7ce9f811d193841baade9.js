9792e4d332bb3334ab4732a7f71d40d9bdb3998b
YuanZheng Hu <bhu078@uottawa.ca>
2019-12-19 02:33:08 -0500

    build

---
+++ build/precache-manifest.d98977c37db7ce9f811d193841baade9.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0740cb1356afb67a956bbfe986fae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b1b2ef815f7a890a4c8",
+    "url": "/static/css/main.f20747b2.chunk.css"
+  },
+  {
+    "revision": "38116dcc5b190e7a475e",
+    "url": "/static/js/2.c7e5c61f.chunk.js"
+  },
+  {
+    "revision": "4b1b2ef815f7a890a4c8",
+    "url": "/static/js/main.36d8fd9b.chunk.js"
+  },
+  {
+    "revision": "dce69fb2a9ba66dd960e",
+    "url": "/static/js/runtime~main.012e954d.js"
+  },
+  {
+    "revision": "b19dae08efb1e168bf019607f14fbe96",
+    "url": "/static/media/Spiral.b19dae08.png"
+  },
+  {
+    "revision": "e96f0db48add1f92e7bb4f2d12a5a2bf",
+    "url": "/static/media/air_cooler.e96f0db4.png"
+  },
+  {
+    "revision": "48ceeab194670c1f739eccb846c01685",
+    "url": "/static/media/birds.48ceeab1.jpg"
+  },
+  {
+    "revision": "60abe483514b39f0332b0a892e503e8b",
+    "url": "/static/media/bussniess_certification.60abe483.jpg"
+  },
+  {
+    "revision": "a74fd75405363e6ad8421ffa59baa259",
+    "url": "/static/media/env_admin.a74fd754.jpg"
+  },
+  {
+    "revision": "409d29baa58bb2bdb5c025baeee4d4ef",
+    "url": "/static/media/factory.409d29ba.png"
+  },
+  {
+    "revision": "5be7ff508239bbb30269f75f87e9cc6c",
+    "url": "/static/media/factoryInside.5be7ff50.jpg"
+  },
+  {
+    "revision": "cc8d834a062fb127790edc22dfc7f133",
+    "url": "/static/media/forest.cc8d834a.jpg"
+  },
+  {
+    "revision": "c327e2714b85d23848c8df0659878a00",
+    "url": "/static/media/forest2.c327e271.jpg"
+  },
+  {
+    "revision": "cee66e29636028c57476146968c64d73",
+    "url": "/static/media/gasket_Plate.cee66e29.jpg"
+  },
+  {
+    "revision": "22fcedf71762e3d4e60bb3c6e09a0caf",
+    "url": "/static/media/health_certificate.22fcedf7.jpg"
+  },
+  {
+    "revision": "b2198c021f9a2c4ef53789286df4e105",
+    "url": "/static/media/iso9000.b2198c02.jpg"
+  },
+  {
+    "revision": "69b499f24324d6fde15a1c5c1a136a68",
+    "url": "/static/media/machine.69b499f2.jpg"
+  },
+  {
+    "revision": "79adda57db5d68979e8980974736e641",
+    "url": "/static/media/moon.79adda57.jpg"
+  },
+  {
+    "revision": "d3054933f8496d91356c6631b1dbf352",
+    "url": "/static/media/permission.d3054933.jpg"
+  },
+  {
+    "revision": "3f8dfe8ae72e4877da5d47252f311551",
+    "url": "/static/media/plate_gas.3f8dfe8a.jpg"
+  },
+  {
+    "revision": "79e1ac782e398240bb788ec4d2525df4",
+    "url": "/static/media/tube.79e1ac78.jpg"
+  },
+  {
+    "revision": "8bcd2e3cafa8f9cc81662ca08304db9c",
+    "url": "/static/media/tude.8bcd2e3c.jpg"
+  },
+  {
+    "revision": "50a71a05abc48b4723a6940ea4184481",
+    "url": "/static/media/welded_plate.50a71a05.jpg"
+  },
+  {
+    "revision": "5590361b38c8566cbd890724a0f8a6c4",
+    "url": "/static/media/welded_plate1.5590361b.jpg"
+  },
+  {
+    "revision": "603bd048ab0dce44ab7987e405f7deec",
+    "url": "/static/media/welded_plate2.603bd048.jpg"
+  }
+]);
---
+++ tensorflow/textTensor.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+
+import tensorflow as tf
+
+# !pip install -q tensorflow-hub
+# !pip install -q tensorflow-datasets
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
